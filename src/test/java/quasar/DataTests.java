e3c097d636dcafe119aecbd024421c6885d9ccd9
objectDisorientedProgrammer <tikiman92@gmail.com>
2020-08-21 23:54:41 -0700

    Generated more test stubs for the Data class

--- src/test/java/quasar/DataTests.java
+++ src/test/java/quasar/DataTests.java
@@ -1,6 +1,6 @@
-/*
- * Created: August 21, 2020
- * 
+/**
+   Created: August 21, 2020
+
     The MIT License (MIT)
     
     Copyright (c) 2013 Gamma (Douglas Chidester, James Howard, Steve Corbette)
@@ -26,21 +26,74 @@
 
 package quasar;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-public class DataTests
+class DataTests
 {
+
     @Test
-    @DisplayName("Default title")
-    void defaultConstructor()
+    void testData()
     {
         Data d = new Data();
-        assertEquals("no title", d.getTitle(), "default title should be 'no title'");
+        assertNotNull(d);
+        assertEquals("no title", d.getTitle());
+        assertEquals(Quasar.ALL, d.getType());
     }
-}
 
+    @Test
+    void testDataInt()
+    {
+        Data d = new Data(Quasar.ALL);
+        assertNotNull(d);
+        assertEquals(Quasar.ALL, d.getType());
+        
+        Data d2 = new Data(Quasar.CONTACT);
+        assertNotNull(d2);
+        assertEquals(Quasar.CONTACT, d.getType());
+        
+        Data d3 = new Data(Quasar.DOCUMENT);
+        assertNotNull(d3);
+        assertEquals(Quasar.DOCUMENT, d.getType());
+        
+        Data d4 = new Data(Quasar.PICTURE);
+        assertNotNull(d4);
+        assertEquals(Quasar.PICTURE, d.getType());
+        
+        Data d5 = new Data(Quasar.WEBSITE);
+        assertNotNull(d5);
+        assertEquals(Quasar.WEBSITE, d.getType());
+    }
+
+    @Test
+    void testDataStringStringStringStringInt()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetKeywords()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetKeywords()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testToString()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testToSaveString()
+    {
+        fail("Not yet implemented");
+    }
+
+}
